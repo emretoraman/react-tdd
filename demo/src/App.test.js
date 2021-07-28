b56da83bef39edf0871a438a2c56054a171a3160
Emre Toraman <emretoraman@gmail.com>
2021-07-28 20:46:16 +0300

    Section 1: Introduction to Jest, Enzyme and TDD

---
+++ demo/src/App.test.js
@@ -0,0 +1,10 @@
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+import Enzyme, { shallow } from 'enzyme'
+import App from './App'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('renders non-empty component without crashing', () => {
+	const wrapper = shallow(<App />)
+	expect(wrapper.exists()).toBe(true)
+})
