3b480df336543dae84792cd56ce90ed9126c6c96
Emre Toraman <emretoraman@gmail.com>
2021-07-29 01:47:44 +0300

    Section 2: Simple React App: Click Counter

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,9 +1,44 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+	const [count, setCount] = useState(0)
+	const [isAlertVisible, setIsAlertVisible] = useState(false)
+
+	const increment = () => setCount(count + 1)
+
+	const decrement = () => {
+		if (count === 0) {
+			setIsAlertVisible(true)
+			return
+		}
+		setCount(count - 1)
+	}
+
+	const renderedAlert = isAlertVisible && count === 0
+		? (
+			<h2
+				data-test="alert"
+				style={{ color: "red" }}
+			>
+				Counter can't go below 0
+			</h2>
+		)
+		: null
+
 	return (
 		<div data-test="component-app">
-			<h1>App</h1>
+			<h1 data-test="counter-display">
+				The counter is currently&nbsp;
+				<span data-test="count">{count}</span>
+			</h1>
+			<button data-test="increment-button" onClick={increment}>
+				Increment counter
+			</button>
+			<button data-test="decrement-button" onClick={decrement}>
+				Decrement counter
+			</button>
+			{renderedAlert}
 		</div>
 	)
 }
--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -5,24 +5,92 @@ import App from './App'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+/**
+  *  Factory function to create a ShallowWrapper for the App component
+  * @function setup
+  * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />)
+
+const findByTestAttr = (wrapper, val) => 
+	wrapper.find(`[data-test="${val}"]`)
+
 test('renders without error', () => {
-	const wrapper = shallow(<App />)
-	const appComponent = wrapper.find('[data-test="component-app"]')
+	const wrapper = setup()
+	const appComponent = findByTestAttr(wrapper, 'component-app')
 	expect(appComponent.length).toBe(1)
 })
 
 test('renders increment button', () => {
-
+	const wrapper = setup()
+	const button = findByTestAttr(wrapper, 'increment-button')
+	expect(button.length).toBe(1)
 })
 
 test('renders counter display', () => {
-
+	const wrapper = setup()
+	const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+	expect(counterDisplay.length).toBe(1)
 })
 
-test('counter display starts at 0', () => {
-
+test('counter starts at 0', () => {
+	const wrapper = setup()
+	const count = Number(findByTestAttr(wrapper, 'count').text())
+	expect(count).toBe(0)
 })
 
-test('clicking button increments counter display', () => {
+test('clicking increment button increments counter', () => {
+	const wrapper = setup()
 
+	const button = findByTestAttr(wrapper, 'increment-button')
+	button.simulate('click')
+
+	const count = Number(findByTestAttr(wrapper, 'count').text())
+	expect(count).toBe(1)
+})
+
+test('clicking decrement button decrements counter', () => {
+	const wrapper = setup()
+
+	const incrementButton = findByTestAttr(wrapper, 'increment-button')
+	incrementButton.simulate('click')
+
+	const decrementButton = findByTestAttr(wrapper, 'decrement-button')
+	decrementButton.simulate('click')
+
+	const count = Number(findByTestAttr(wrapper, 'count').text())
+	expect(count).toBe(0)
+})
+
+test('counter doesn\'t get below 0', () => {
+	const wrapper = setup()
+
+	const button = findByTestAttr(wrapper, 'decrement-button')
+	button.simulate('click')
+
+	const count = Number(findByTestAttr(wrapper, 'count').text())
+	expect(count).toBe(0)
+})
+
+test('clicking decrement button when counter is 0 shows alert', () => {
+	const wrapper = setup()
+
+	const button = findByTestAttr(wrapper, 'decrement-button')
+	button.simulate('click')
+
+	const alert = findByTestAttr(wrapper, 'alert')
+	expect(alert.length).toBe(1)
+})
+
+test('alert gets dismissed if counter is greater than 0', () => {
+	const wrapper = setup()
+
+	const decrementButton = findByTestAttr(wrapper, 'decrement-button')
+	decrementButton.simulate('click')
+
+	const incrementButton = findByTestAttr(wrapper, 'increment-button')
+	incrementButton.simulate('click')
+
+	const alert = findByTestAttr(wrapper, 'alert')
+	expect(alert.length).toBe(0)
 })
